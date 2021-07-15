a7c39a5332066ef6428c6b85bc77a5cf7ad03b1d
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-15 05:32:10 -0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET')
 
 # Configure Library to use SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bqucsxtgiaflnu:5d54470ffd2d712c5bf3097db3b11de74543613f4911ff1ed6af05ebc0d9ac93@ec2-54-157-100-65.compute-1.amazonaws.com:5432/d1qschqsh1ocns'
+# os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 # Google Books API
